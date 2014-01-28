d5d893c9d211de96e3164280a6d4fb245ea69045
Andre Smith <andrepsmithjr@gmail.com>
2014-01-28 12:40:57 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Qna::Application.config.secret_key_base = 'd203a3e6fb0accb95db37696cb745d5eb3f3c2f90384296d176118710640fabe709c20b248c8f82435d9c64e43b48ce4baa8dbd46635e3dd2d63a5380549b78a'
